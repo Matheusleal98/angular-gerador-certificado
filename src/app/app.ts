66452676f8f28e15b583713cb2b0937f696e79b4
Matheus Leal <matheussleal98@gmail.com>
2025-10-31 14:47:13 -0300

    feat: add base component

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,10 +4,11 @@ import { Navbar } from './_components/navbar/navbar';
 import { PrimaryButton } from './_components/primary-button/primary-button';
 import { SecondaryButton } from './_components/secondary-button/secondary-button';
 import { ItemCertificado } from './_components/item-certificado/item-certificado';
+import { Base } from './_components/base/base';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, PrimaryButton, SecondaryButton, ItemCertificado],
+  imports: [RouterOutlet, Navbar, PrimaryButton, SecondaryButton, ItemCertificado, Base],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
