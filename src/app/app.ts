25b19c4daedb176775b0b622456ffc8430a559fb
Matheus Leal <matheussleal98@gmail.com>
2025-10-31 17:21:43 -0300

    add certificado page base

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,10 +5,11 @@ import { Navbar } from './_components/navbar/navbar';
 import { Base } from './_components/base/base';
 import { Certificados } from './pages/certificados/certificados';
 import { CertificadoForm } from './pages/certificado-form/certificado-form';
+import { Certificado } from './pages/certificado/certificado';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, Base, Certificados, CertificadoForm],
+  imports: [RouterOutlet, Navbar, Base, Certificados, CertificadoForm, Certificado],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
