8bf01191dd35c5e266141524eff3d2d745f83142
Matheus Leal <matheussleal98@gmail.com>
2025-10-31 15:20:15 -0300

    feat: add certificados page

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './_components/navbar/navbar';
-import { PrimaryButton } from './_components/primary-button/primary-button';
-import { SecondaryButton } from './_components/secondary-button/secondary-button';
-import { ItemCertificado } from './_components/item-certificado/item-certificado';
 import { Base } from './_components/base/base';
+import { Certificados } from './pages/certificados/certificados';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, PrimaryButton, SecondaryButton, ItemCertificado, Base],
+  imports: [RouterOutlet, Navbar, Base, Certificados],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
