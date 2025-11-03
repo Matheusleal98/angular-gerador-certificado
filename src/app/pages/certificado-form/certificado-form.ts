c6dc4012f626874fcf9a8238dab51555d38824fe
Matheus Leal <matheussleal98@gmail.com>
2025-11-03 20:16:45 -0300

    ajustes validações campos formulários

--- src/app/pages/certificado-form/certificado-form.ts
+++ src/app/pages/certificado-form/certificado-form.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { SecondaryButton } from '../../_components/secondary-button/secondary-button';
 import { PrimaryButton } from '../../_components/primary-button/primary-button';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, type NgControl } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-certificado-form',
-  imports: [SecondaryButton,  PrimaryButton, FormsModule],
+  imports: [SecondaryButton,  PrimaryButton, FormsModule, CommonModule],
   templateUrl: './certificado-form.html',
   styleUrl: './certificado-form.css',
 })
@@ -13,4 +14,8 @@ export class CertificadoForm {
   nome: string = '';
   atividade: string = '';
   atividades: string[] = ['Angular', 'React'];
+
+  validarCampo(control: NgControl) {
+    return control.invalid && control.touched;
+  }
 }
